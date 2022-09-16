c1460578376df589e97f50e29eaaf5388a3cb3c4
damilola <damilolaibiezugbe@gmail.com>
2022-09-16 10:09:14 -0400

    AWS-123-paula: added internet gateway, route table, and changed the subnet route table

--- dammy.tf
+++ dammy.tf
@@ -13,9 +13,34 @@ resource "aws_vpc" "ife-vpc" {
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.0.1/24"
 
   tags = {
     Name = "Main"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.ife-vpc.id
+
+  tags = {
+    Name = "ife-igw"
+  }
+}
+
+resource "aws_route_table" "ife-route-table" {
+  vpc_id = aws_vpc.ife-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id             = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "ife-rt"
   }
 }
